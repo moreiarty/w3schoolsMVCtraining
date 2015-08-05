3381be0e8467aa29e0e8d82e20b65999bed59000
unknown <pranavm@Pranav-PC.in.freshview>
2015-08-05 16:55:42 +1000

    added local files from initial ASP.NET MVC application

---
+++ W3Tutorial_ASPNETMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="W3Tutorial_ASPNETMVC.MvcApplication" Language="C#" %>
---
+++ W3Tutorial_ASPNETMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(W3Tutorial_ASPNETMVC.Startup))]
+namespace W3Tutorial_ASPNETMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
