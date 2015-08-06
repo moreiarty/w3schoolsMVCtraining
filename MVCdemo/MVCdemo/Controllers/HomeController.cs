5560e437ea0a95a71fb115cc8087e81230c5de40
unknown <pranavm@Pranav-PC.in.freshview>
2015-08-06 14:51:48 +1000

    no message

--- MVCdemo/MVCdemo/Controllers/HomeController.cs
+++ MVCdemo/MVCdemo/Controllers/HomeController.cs
@@ -4,27 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace MVCdemo.Controllers
+namespace MvcDemo.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
-            return View();
+        { 
+            return View(); 
         }
 
         public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+        { 
+            return View(); 
         }
     }
 }
